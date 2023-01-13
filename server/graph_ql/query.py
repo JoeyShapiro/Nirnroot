cd9ae061752aed4f05469ad36be56df5f0f5e500
JoeyShapiro <joeyashapiro@gmail.com>
2023-01-12 22:48:50 -0600

    super CLEAN

--- server/graph_ql/query.py
+++ server/graph_ql/query.py
@@ -8,23 +8,24 @@ class Query(graphene.ObjectType):
     get_journals = graphene.List(Quest)
     get_root = graphene.List(Quest)
 
-    @staticmethod
+    @staticmethod # this whole thing is the CLEANEST SHIT youve ever done seen
     def resolve_quests(parent, info, **kargs):
         pargs = ['id', 'type', 'parentId']
         quests_query = Quest.get_query(info) # rename to filtered or seomthing
 
         # check for the args
-        if kargs.get('id') is not None:
-            quests_query = quests_query.filter(TableQuests.id == kargs.get('id')).all()
+        for arg in pargs:
+            if kargs.get(arg) is not None:
+                quests_query = quests_query.filter(getattr(TableQuests, arg) == kargs.get(arg)).all()
         
         return quests_query
     
-    @staticmethod
+    @staticmethod # unneeded
     def resolve_get_journals(parent, info, **args):
         quests_query = Quest.get_query(info)
         return quests_query.filter(TableQuests.type == "journal").all()
     
-    @staticmethod
+    @staticmethod # unneeded
     def resolve_get_root(parent, info, **args):
         quests_query = Quest.get_query(info)
         return quests_query.filter(TableQuests.parent_id == 0).all()
