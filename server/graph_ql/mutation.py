066f3a24e1cc652281a825f6e2e18feaa3e48f66
JoeyShapiro <joeyashapiro@gmail.com>
2023-01-14 11:48:42 -0600

    clean up

--- server/graph_ql/mutation.py
+++ server/graph_ql/mutation.py
@@ -2,7 +2,6 @@ import graphene
 from database import db_session, TableQuests
 from graph_ql.typedefs import Quest, SaveQuestFields
 from sqlalchemy import update
-import sys
 
 
 class SaveQuest(graphene.Mutation):
@@ -17,14 +16,12 @@ class SaveQuest(graphene.Mutation):
     def mutate(self, info, input):
         quest = TableQuests(**input) # this is the new one
         if (dquest := db_session.get(TableQuests, quest.id)) is not None: # also checks if the id is valid
-            vals = {}
-            for col in SaveQuest.updatable:
-                vals[col] = input.get(col) or getattr(dquest, col)
+            vals = { col: input.get(col) or getattr(dquest, col) for col in SaveQuest.updatable } # dict for quest
             db_session.execute(
                 update(TableQuests).where(TableQuests.id == dquest.id).values(vals)
             )
         else:
-            db_session.add(quest)
+            db_session.add(quest) # should block task on root
         
         db_session.commit()
 
